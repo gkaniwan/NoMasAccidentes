8a00af89767001c09e66f586c5b1752fbc89d4c0
gkaniwan <gonzalo@kaniwan.cl>
2020-04-17 22:57:51 -0400

    todos mantenedores administrador

---
+++ mantenedorCasos.php
@@ -0,0 +1,366 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>No Mas Accidentes | Mantenedor Casos</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- overlayScrollbars -->
+  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+  <!-- Google Font: Source Sans Pro -->
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+</head>
+<body class="hold-transition sidebar-mini">
+<!-- Site wrapper -->
+<div class="wrapper">
+  <!-- Navbar -->
+  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+      </li>
+<!--       <li class="nav-item d-none d-sm-inline-block">
+        <a href="index3.html" class="nav-link">Home</a>
+      </li>
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="#" class="nav-link">Contact</a>
+      </li> -->
+    </ul>
+
+
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+      <!-- Messages Dropdown Menu -->
+<!--       <li class="nav-item dropdown">
+        <a class="nav-link" data-toggle="dropdown" href="#">
+          <i class="far fa-comments"></i>
+          <span class="badge badge-danger navbar-badge">3</span>
+        </a>
+        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+          <a href="#" class="dropdown-item"> -->
+            <!-- Message Start -->
+<!--             <div class="media">
+              <img src="dist/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle">
+              <div class="media-body">
+                <h3 class="dropdown-item-title">
+                  Brad Diesel
+                  <span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
+                </h3>
+                <p class="text-sm">Call me whenever you can...</p>
+                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+              </div>
+            </div> -->
+            <!-- Message End -->
+<!--           </a>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item"> -->
+            <!-- Message Start -->
+<!--             <div class="media">
+              <img src="dist/img/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
+              <div class="media-body">
+                <h3 class="dropdown-item-title">
+                  John Pierce
+                  <span class="float-right text-sm text-muted"><i class="fas fa-star"></i></span>
+                </h3>
+                <p class="text-sm">I got your message bro</p>
+                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+              </div>
+            </div> -->
+            <!-- Message End -->
+<!--           </a>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item"> -->
+            <!-- Message Start -->
+<!--             <div class="media">
+              <img src="dist/img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
+              <div class="media-body">
+                <h3 class="dropdown-item-title">
+                  Nora Silvester
+                  <span class="float-right text-sm text-warning"><i class="fas fa-star"></i></span>
+                </h3>
+                <p class="text-sm">The subject goes here</p>
+                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+              </div>
+            </div> -->
+            <!-- Message End -->
+<!--           </a>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
+        </div>
+      </li> -->
+      <!-- Notifications Dropdown Menu -->
+<!--       <li class="nav-item dropdown">
+        <a class="nav-link" data-toggle="dropdown" href="#">
+          <i class="far fa-bell"></i>
+          <span class="badge badge-warning navbar-badge">15</span>
+        </a>
+        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+          <span class="dropdown-item dropdown-header">15 Notifications</span>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item">
+            <i class="fas fa-envelope mr-2"></i> 4 new messages
+            <span class="float-right text-muted text-sm">3 mins</span>
+          </a>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item">
+            <i class="fas fa-users mr-2"></i> 8 friend requests
+            <span class="float-right text-muted text-sm">12 hours</span>
+          </a>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item">
+            <i class="fas fa-file mr-2"></i> 3 new reports
+            <span class="float-right text-muted text-sm">2 days</span>
+          </a>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
+        </div>
+      </li> -->
+      <li class="nav-item">
+        <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
+          <i class="fas fa-th-large"></i>
+        </a>
+      </li>
+    </ul>
+  </nav>
+  <!-- /.navbar -->
+
+  <!-- Main Sidebar Container -->
+  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="inicioAdmin.php" class="brand-link">
+      <img src="dist/img/noMasAccidentesLogo.jfif"
+           alt="AdminLTE Logo"
+           class="brand-image img-circle elevation-3"
+           style="opacity: .8">
+      <span class="brand-text font-weight-light">No mas Accidentes</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user (optional) -->
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+          <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+        </div>
+        <div class="info">
+          <a href="#" class="d-block">Nombre Usuario</a>
+        </div>
+      </div>
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+          <li class="nav-item has-treeview">
+            <a href="inicioAdmin.php" class="nav-link">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Reportes
+                <!-- <i class="right fas fa-angle-left"></i> -->
+              </p>
+            </a>
+          </li>
+
+          <li class="nav-item has-treeview">
+            <a  class="nav-link">
+              <i class="nav-icon fas fa-edit"></i>
+              <p>
+                Mantenedores
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="mantenedorClientes.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Clientes</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="mantenedorProfesionales.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Profesionales</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="mantenedorContratos.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Contratos</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="mantenedorPagos.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Pagos</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="mantenedorActividades.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Actividades</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="mantenedorAccidentes.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Accidentes</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="mantenedorChecklist.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Checklist</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="mantenedorCasos.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Casos</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Mantenedor Casos</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="inicioAdmin.php">Inicio</a></li>
+              <li class="breadcrumb-item active">Mantenedor Casos</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+
+        <div class="row">
+
+         <div class="col-lg-12">
+
+            <div class="card">
+              <div class="card-header border-0">
+                <h3 class="card-title">Casos</h3>
+                <div class="card-tools">
+                  <button type="button" class="btn btn-sm btn-block btn-outline-success" onclick="verCrearCaso();">Agregar</button>
+                </div>
+              <div class="card-body table-responsive p-0">
+                <table class="table table-striped table-valign-middle">
+                  <thead>
+                  <tr>
+                    <th>ID</th>
+                    <th>Descripción</th>
+                    <th>Cliente</th>
+                    <th>Profesional</th>
+                    <th>Fecha</th>
+                    <th></th>
+                    <th></th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>1</td>
+                      <td>Caso 1</td>
+                      <td>Cliente 5</td>
+                      <td>Profesional 3</td>
+                      <td>05-04-2020</td>
+                      <td>
+                        <button type="button" class="btn btn-sm btn-block btn-outline-warning" onclick="verActualizarCaso();">Modificar</button>
+                      </td>
+                      <td>
+                        <button type="button" class="btn btn-sm btn-block btn-outline-danger">Eliminar</button>
+                      </td>
+                    </tr>
+                     
+                  </tbody>
+                </table>
+              </div>
+            </div>
+            <!-- /.card -->
+          </div>
+          <!-- /.col-md-6 -->
+        </div>
+
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+  <?php include('modalActualizarCaso.php');?>
+  <?php include('modalNuevoCaso.php');?>
+
+  <footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+      <b>Version</b> 1.0
+    </div>
+    <strong>Copyright &copy; 2020 <a href="login.php">No mas Accidentes</a>.</strong> Todos Los Derechos Reservados
+  </footer>
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="dist/js/demo.js"></script>
+
+
+<!-- OPTIONAL SCRIPTS -->
+<script src="plugins/chart.js/Chart.min.js"></script>
+<script src="dist/js/demo.js"></script>
+<script src="dist/js/pages/dashboard3.js"></script>
+
+<script>
+  
+  function verActualizarCaso(){
+
+    $('#modalActualizarCaso').modal('show');
+
+  }
+
+  function verCrearCaso(){
+
+    $('#modalNuevoCaso').modal('show');
+
+  }
+
+</script>
+
+
+
+
+</body>
+</html>
---
+++ modalActualizarCaso.php
@@ -0,0 +1,69 @@
+      <div class="modal fade" id="modalActualizarCaso">
+        <div class="modal-dialog modal-lg">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h4 class="modal-title">Actualizar Caso</h4>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+
+            <!-- Horizontal Form -->
+            <div class="card card-info">
+              <!-- form start -->
+              <form class="form-horizontal">
+                <div class="card-body">
+                  <div class="form-group row">
+                    <label for="nombreClienteActividad" class="col-sm-2 col-form-label">Cliente</label>
+                    <div class="col-sm-10">
+                      <select class="form-control" id="nombreClienteActividad">
+                        <option value="cliente1">Cliente 1</option>
+                        <option value="cliente2">Cliente 2</option>
+                        <option value="cliente3">Cliente 3</option>
+                        <option value="cliente4">Cliente 4</option>
+                        <option value="cliente5">Cliente 5</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div class="form-group row">
+                    <label for="nombreProfesionalActividad" class="col-sm-2 col-form-label">Profesional</label>
+                    <div class="col-sm-10">
+                      <select class="form-control" id="nombreProfesionalActividad">
+                        <option value="profesional1">Profesional 1</option>
+                        <option value="profesional2">Profesional 2</option>
+                        <option value="profesional3">Profesional 3</option>
+                        <option value="profesional4">Profesional 4</option>
+                        <option value="profesional5">Profesional 5</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div class="form-group row">
+                    <label for="fechaNuevoAccidente" class="col-sm-2 col-form-label">Fecha</label>
+                    <div class="col-sm-10">
+                      <input type="date" class="form-control" id="fechaNuevoAccidente">
+                    </div>
+                  </div>
+                  <div class="form-group row">
+                    <label for="descripcionNuevaActividad" class="col-sm-2 col-form-label">Descripción</label>
+                    <div class="col-sm-10">
+                      <input type="text" class="form-control" id="descripcionNuevaActividad" placeholder="Descripción accidente">
+                    </div>
+                  </div>
+                </div>
+                <!-- /.card-body -->
+              </form>
+            </div>
+            <!-- /.card -->
+              
+            </div>
+            <div class="modal-footer justify-content-between">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+              <button type="button" class="btn btn-success">Guardar</button>
+            </div>
+          </div>
+          <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+      </div>
+      <!-- /.modal -->
